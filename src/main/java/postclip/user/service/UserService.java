a3a3e7b75dce6abce58535d4e1a5978a857a9df2
Sebastian Moreno <sdmorenob@unal.edu.co>
2017-03-11 00:53:12 -0500

    Include Java users logic

---
+++ src/main/java/postclip/user/service/UserService.java
@@ -0,0 +1,47 @@
+package postclip.user.service;
+
+import postclip.user.model.User;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+
+@Stateless
+public class UserService{
+
+    @PersistenceContext
+    EntityManager entityManager;
+
+    public List<User> getAllUsers( int first, int maxResult ){
+        return entityManager.createNamedQuery( User.FIND_ALL )
+                .setFirstResult( first ).setMaxResults( maxResult ).getResultList( );
+    }
+
+    public User getUserById( long id ){
+        return entityManager.find( User.class, id );
+    }
+
+    public void createUser( User user ){
+        entityManager.persist( user );
+    }
+
+    public User updateUser( long id, User user ){
+        User userToUpdate = entityManager.find( User.class, id );
+        userToUpdate.setName( user.getName( ) );
+        userToUpdate.setEmail( user.getEmail( ) );
+        return entityManager.merge( userToUpdate );
+    }
+
+    public User updatePassword( long id, String password ){
+        User userToUpdate = entityManager.find( User.class, id );
+        userToUpdate.setPassword( password );
+        return entityManager.merge( userToUpdate );
+    }
+
+    public void deleteUser( long id ){
+        User user = entityManager.find( User.class, id );
+        entityManager.remove( user );
+    }
+}
